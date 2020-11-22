25ad3c8057c3da61d8f3d247055c5e225729a1c4
BrandedNomad <info@brandednomad.com>
2020-11-22 14:34:45 +0700

    feat: add delete file feature to endpoint

--- src/server.ts
+++ src/server.ts
@@ -13,27 +13,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+
+
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   app.get("/filteredimage", async (req,res)=>{
-
-   // res.status(200).send("you made it")
-
-    //    1. validate the image_url query
-
+   // init file variable
+    let file;
+    //1. validate the image_url query
     if(req.query.image_url){
-
       // 2. call filterImageFromURL(image_url) to filter the image
       const image_url = req.query.image_url
-      console.log(image_url)
-      const file = await filterImageFromURL(image_url)
-
-
+      file = await filterImageFromURL(image_url)
       // 3. send the resulting file in the response
       res.status(200).sendFile(file) //might have to change ot sendFile
-
       // 4. deletes any files on the server on finish of the response
-      //todo: figure this out
+      deleteLocalFiles([file])
     }else{
       res.status(404).send("Image URL does not exist")
     }
--- src/util/util.ts
+++ src/util/util.ts
@@ -42,7 +42,9 @@ export function filterImageFromURL2(inputURL: string): any {
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+    setTimeout(()=>{
+        for( let file of files) {
+            fs.unlinkSync(file);
+        }
+    },10000)
 }
